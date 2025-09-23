e5df2e5a5cf4497d1ebcb86ebe3f1f898e7801d4
islam <islammuhd8@gmail.com>
2025-09-23 17:00:18 +0100

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import AboutPic from "../assets/about.jpg"
 import { motion } from "framer-motion"
+import { ABOUT_TEXT } from "../constants"
 const About = () => {
     return (
         <div className="border-b border-neutral-900 pd-4">
@@ -15,11 +16,17 @@ const About = () => {
                         <img className="rounded-2xl w-100" src={AboutPic} alt="" />
                     </div>
                 </motion.div>
-                <div className="w-full lg:w-1/2">
+                <motion.div 
+                whileInView={{ opacity: 1, x: 0}}
+                initial={{ opacity: 0, x: 100}}
+                transition={{duration: 1}}
+                className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:justify-start">
-                        <p className="my-2 max-w-xl py-6">I am a dedicated and versatile Frontend developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including HTML, CSS, javascript, React, and Next.js. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.</p>
+                        <p className="my-2 max-w-xl py-6">
+                            {ABOUT_TEXT}
+                        </p>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
--- src/components/Technoligies.jsx
+++ src/components/Technoligies.jsx
@@ -3,29 +3,68 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { RiFirebaseFill } from "react-icons/ri";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { scale: 0, opacity: 0 },
+  animate: {
+    scale: [1, 1.2, 1], // Oscillate between 100% and 120% scale
+    opacity: 1,
+    transition: {
+      duration: duration,
+      repeat: Infinity, // Loop indefinitely
+      repeatType: 'loop', // Ensures smooth looping
+      ease: 'easeInOut', // Smooth easing for natural motion
+    },
+  },
+});
 const Technoligies = () => {
     return (
         <div className="border-b border-neutral-800 pb-24">
             <h1 className="my-20 text-center text-4xl">Technologies</h1>
-            <div className="flex flex-wrap justify-center items-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiReactjsLine className="text-7xl text-cyan-400 " />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <IoLogoJavascript className="text-7xl text-blue-200" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <div className="flex flex-wrap justify-center items-center gap-8">
+                <motion.div
+                variants={iconVariants(1)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiReactjsLine className="text-6xl text-cyan-400 " />
+                </motion.div>
+                <motion.div 
+                variants={iconVariants(1.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <IoLogoJavascript className="text-6xl text-blue-200" />
+                </motion.div>
+                <motion.div
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaHtml5 className="text-7xl text-blue-700" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiNextjsLine className="text-7xl text-purple-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <FaCss3Alt className="text-7xl text-red-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiFirebaseFill className="text-7xl text-green-400" />
-                </div>
+                </motion.div>
+                <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiNextjsLine className="text-6xl text-purple-400" />
+                </motion.div>
+                <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaCss3Alt className="text-6xl text-red-400" />
+                </motion.div>
+                <motion.div
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate" 
+                className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiFirebaseFill className="text-6xl text-green-400" />
+                </motion.div>
             </div>
         </div>
     )
