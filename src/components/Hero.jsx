91d3b490938368e39dd12311600178b26823b939
islam <islammuhd8@gmail.com>
2025-09-23 15:10:32 +0100

    added motion animation to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,16 @@
 // import { HERO_CONTENT } from "../constants"
 import Logo from '../assets/islam.jpg'
+import { motion } from 'framer-motion'
+import { HERO_CONTENT } from '../constants'
+
+const constainer = (delay) => ({
+    hidden: { x: -100, opacity: 0},
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {duration: 0.5, delay: delay},
+    },
+})
 
 const Hero = () => {
     return (
@@ -7,18 +18,35 @@ const Hero = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pd-16 text-6xl font-sans tracking-tight lg:mt-16 lg:text-8xl">Islam Sani</h1>
-                        <span className=" mt-16 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+                        <motion.h1 
+                        variants={constainer(0.5)}
+                        initial='hidden'
+                        animate='visible'
+                         
+                        className="pd-16 text-6xl font-sans tracking-tight lg:mt-16 lg:text-8xl">Islam Sani</motion.h1>
+                        <motion.span 
+                        variants={constainer(0.9)}
+                        initial='hidden'
+                        animate='visible' className=" mt-16 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                             Frontend Developer
-                        </span>
-                        <p className="my-2 max-w-xl py-6 font-light tracking-tight">
-                           I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like Javascript, React and Next.js, as well as a bit of back-end technologies like Firebase. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.
-                        </p>
+                        </motion.span>
+                        <motion.p 
+                        variants={constainer(1.4)}
+                        initial='hidden'
+                        animate='visible'
+                        
+                        className="my-2 max-w-xl py-6 font-light tracking-tight">
+                           {HERO_CONTENT}
+                        </motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8 ">
                 <div className="flex justify-center">
-                    <img className='h-120 w-80 rounded-2xl' src={Logo} alt="" />
+                    <motion.img 
+                    initial={{x: 100, opacity: 0}}
+                    animate={{x: 0, opacity: 1}}
+                    transition={{duration: 1, delay: 2}}
+                    className='h-120 w-80 rounded-2xl' src={Logo} alt="" />
                 </div>
                 </div>
             </div>
