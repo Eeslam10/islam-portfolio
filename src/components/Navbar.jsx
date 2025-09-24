667cdf13ac3212d0a03e10591911448a503c2c70
islam <islammuhd8@gmail.com>
2025-09-24 11:38:27 +0100

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaSquareXTwitter } from 'react-icons/fa6'
 
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-4'>
+    <nav className='mb-20 flex items-center justify-between py-4 mx-auto max-w-6xl'>
         <div className='flex flex-shrink-0 items-center'>
           <h2 className='text-4xl text-white'>Is</h2>  
         </div>
